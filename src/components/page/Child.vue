5f804f34ba5b5490eb89169e80ec955aff3790f4
han_shy <han_shy@neusoft.com>
2022-02-10 18:05:40 +0800

    update

--- src/components/page/Child.vue
+++ src/components/page/Child.vue
@@ -3,9 +3,9 @@
     <p>Hello World</p>
     <div class="input-group">
       <div class="input-item">
-        <label>loan_total(W): </label>
+        <label>loanTotal(W): </label>
         <input 
-          v-model="loan_total"
+          v-model="loanTotal"
           class="loan-input"
           type="text">
       </div>
@@ -17,17 +17,11 @@
           type="text">
       </div>
       <div>
-        <v-btn
-          color="blue-grey"
-          class="ma-2 white--text"
+        <button
+          type="button"
           @click="calculate()">
           calculate
-          <v-icon
-            right
-            dark>
-            mdi-cloud-upload
-          </v-icon>
-        </v-btn>
+        </button>
         <button
           type="button"
           @click="clearResult()">
@@ -48,7 +42,7 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class Child extends Vue {
-  private loan_total = 0
+  private loanTotal = 0
   private loan_month = 0
   private per_year_rate = 0.05
 
@@ -65,33 +59,32 @@ export default class Child extends Vue {
     const ele = document.createElement('span')
     const innerText = 'destoryed'
     document.body.append(ele)
-    let count = 3
+    let count = 5
     const interval = setInterval(() => {
       if (count < 0) {
         clearInterval(interval)
         ele.remove()
       }
-      ele.innerText = innerText + ' ' + (count--) + '...'
+      const suffix = ['·', '· ·', '· · ·', '· · · ·']
+      ele.innerText = innerText + ' ' + (count--) + ' ' + (count >= 0 ?suffix[(suffix.length - 1) - count % suffix.length]: '')
     }, 1000)
   }
 
   calculate(): void {
     this.result_loan = 0
-    const total = this.loan_total * 10000
+    const total = this.loanTotal * 10000
     const months = this.loan_month
     const per_year_rate = 0.05
     const per_month_rate = Math.round(per_year_rate * 100000 / 12) / 100000
-    // let loan_total = 0
     let loan_overrage = total
-    // let loan_per_month = []
     for (let i = 0; i < months; i ++) {
       loan_overrage = total - (Math.round((total / months))* i)
       this.result_loan += loan_overrage * per_month_rate
       this.result_arr.push(Math.round((total / months)) + loan_overrage * per_month_rate)
     }
     this.isShowResultArea = true
-    // console.log('loan_total: ', loan_total)
-    // console.log('loan_per_month: ', loan_per_month)
+    // console.log('loanTotal: ', this.result_loan)
+    // console.log('loan_per_month: ', this.result_arr)
   }
 
   clearResult(): void {
